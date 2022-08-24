a34d5e126ea9d00a2ef32256fd99b2d1c8d2101a
vegss <veeti.e04@gmail.com>
2022-08-24 17:50:51 +0300

    step 10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,7 +1,7 @@
 import personService from './services/Persons'
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 
+// Filter form
 const Filter = ({filterStr, handleFilterChange}) => {
   return(
   <form>
@@ -10,39 +10,65 @@ const Filter = ({filterStr, handleFilterChange}) => {
   )
 }
 
-const AddPerson = (props) => {
+// Adding person form
+const AddPerson = ({persons, setPersons}) => {
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+
+  const personObject = {
+    name: newName,
+    number: newNum
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumChange = (event) => {
+    setNewNum(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const sameName = persons.find(person => person.name === newName)
+    if (sameName !== undefined) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+        personService
+        .update(personObject, sameName.id)
+        .then(response => {
+          setPersons(persons.map(p => p.id === response.data.id ? response.data : p))
+        })
+      }
+    }
+    else{
+      personService
+      .create(personObject)
+      .then(response => setPersons(persons.concat(response.data)))
+    }
+    setNewName('')
+    setNewNum('')
+  }
+
   return(
-    <form onSubmit={props.addPerson}>
-      <div>name: <input value={props.newName} onChange={props.handleNameChange}/></div>
-      <div>number: <input value={props.newNum} onChange={props.handleNumChange} /></div>
+    <form onSubmit={addPerson}>
+      <div>name: <input value={newName} onChange={handleNameChange}/></div>
+      <div>number: <input value={newNum} onChange={handleNumChange} /></div>
       <div><button type="submit">add</button></div>
     </form>
   )
 }
 
-const Person = ({person, deletePerson, personsToShow}) => {
+const Content = ({persons, deletePerson}) => {
   return(
-    <div key={person.id}>
-      {person.name} {person.number}
-      <button onClick={() => deletePerson(person.id, person.name, personsToShow)}>delete</button>
-    </div>
-)}
-
-const Content = ({personsToShow, deletePerson}) => {
-  return(
-    personsToShow.map(person =>
-      <Person key={person.id} person={person} deletePerson={deletePerson} personsToShow={personsToShow} />
-    )
+    persons.map(person => <p key={person.id}>{person.name} {person.number} <button onClick={() => deletePerson(person)}>delete</button></p>)
   )
 }
 
 const App = () => {
 
   const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNum, setNewNum] = useState('')
   const [filterStr, setFilterStr] = useState('')
-  const [showAll, setShowAll] = useState(true)
+  const [personsToShow, setPersonsToShow] = useState(persons)
 
   useEffect(() => {
     personService
@@ -52,73 +78,39 @@ const App = () => {
     })
   },[])
 
-  const deletePerson = (event, person) => {
-
+  const deletePerson = (person) => {
     if (window.confirm(`Delete ${person.name}`)) {
-      axios
-      .delete(`http://localhost:3001/persons/${person.id}`)
-      .then(response => {
-        setPersons(persons.map(p => p.id !== person.id ? p : response.data))
-        personsToShow = showAll 
-          ? persons 
-          : persons.filter(person => person.name.toLowerCase().includes(filterStr))
+      personService
+      .remove(person.id)
+      .then(() => {
+        personService
+        .getAll()
+        .then(response => {
+          setPersons(response.data)
+        })
       })
     }
   }
 
-  const addPerson = (event) => {
-    event.preventDefault()
-
-    const sameNames = persons.filter(person => person.name === newName)
-    if (sameNames.length > 0) {
-      return(alert(`${newName} is already added to phonebook`))
-    }
-
-    const personObject = {
-      name: newName,
-      number: newNum
-    }
-    personService
-      .create(personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNum('')
-      })
-
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumChange = (event) => {
-    setNewNum(event.target.value)
-  }
   const handleFilterChange = (event) => {
     setFilterStr(event.target.value)
-    if (filterStr.length > 0) {
-      setShowAll(false)
-    }
-    else{ 
-      setShowAll(true)
-    }
-  } 
-
-  const personsToShow = showAll 
-    ? persons 
-    : persons.filter(person => person.name.toLowerCase().includes(filterStr))
+    setPersonsToShow(persons.filter(person => person.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1))
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
         <Filter filterStr={filterStr} handleFilterChange={handleFilterChange}/>
       <h2>add a new</h2>  
-        <AddPerson newName={newName} newNum={newNum} handleNameChange={handleNameChange} handleNumChange={handleNumChange} addPerson={addPerson}/>
+        <AddPerson persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-        <Content personsToShow={personsToShow} deletePerson={deletePerson}/>
+        {
+          filterStr === '' 
+            ? <Content deletePerson={deletePerson} persons={persons}/> 
+            : <Content deletePerson={deletePerson} persons={personsToShow}/>
+        }
     </div>
   )
-
 }
 
 export default App
--- osa2/puhelinluettelo/src/services/Persons.js
+++ osa2/puhelinluettelo/src/services/Persons.js
@@ -9,6 +9,14 @@ const create = (newObject) => {
     return axios.post(baseUrl, newObject)
 };
 
-const personService = {getAll, create}
+const remove = (id) => {
+    return axios.delete(`${baseUrl}/${id}`)
+};
+
+const update = (personObject, id) => {
+    return axios.put(`${baseUrl}/${id}`, personObject)
+}
+
+const personService = {getAll, create, remove, update}
 
 export default personService
