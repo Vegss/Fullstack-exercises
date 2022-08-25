6871e25b13312ce198711178e0a5b55295eed5ae
vegss <veeti.e04@gmail.com>
2022-08-25 12:08:14 +0300

    step 7

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 
@@ -29,7 +28,6 @@ let persons = [
 
   app.use(express.json())
   app.use(morgan('tiny'))
-  app.use(cors())
 
   app.get('/info', (req, res) => {
     const current = Date()
