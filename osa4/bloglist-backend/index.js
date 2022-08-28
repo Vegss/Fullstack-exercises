684e312194eb29cebde3229f251c5c59d29406c9
vegss <veeti.e04@gmail.com>
2022-08-28 19:46:39 +0300

    Added eslint

--- osa4/bloglist-backend/index.js
+++ osa4/bloglist-backend/index.js
@@ -2,7 +2,6 @@ const config = require('./utils/config')
 const { info } = require('./utils/logger')
 const app = require('./app')
 const http = require('http')
-const express = require('express')
 
 const server = http.createServer(app)
 
--- osa4/bloglist-backend/models/blog.js
+++ osa4/bloglist-backend/models/blog.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
 module.exports = mongoose.model('Blog', blogSchema)
