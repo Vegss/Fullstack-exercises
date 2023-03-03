dddf698f48f2edbb7defa04452d7521f616e91e7
vegss <veeti.e04@gmail.com>
2023-03-03 16:05:51 +0200

    4.18

--- osa4/bloglist/server/controllers/blogs.js
+++ osa4/bloglist/server/controllers/blogs.js
@@ -1,21 +1,34 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 require('express-async-errors')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const { author, title, url, likes } = request.body
-  const newBlog = new Blog({
+  const { author, title, url, likes, userId } = request.body
+
+  if (!userId) return response.status(400).json({ error: "No userId given." })
+
+  const user = await User.findById(userId)
+
+  const blog = new Blog({
     author: author,
     title: title,
     url: url,
-    likes: likes
+    likes: likes,
+    user: user
   })
-  const blog = await newBlog.save()
+
+  console.log(blog)
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(blog)
 })
 
