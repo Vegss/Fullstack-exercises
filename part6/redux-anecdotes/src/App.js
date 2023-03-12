e41cc6d752001c77efcdf546ed0da57e485f3aa0
vegss <veeti.e04@gmail.com>
2023-03-12 19:45:04 +0200

    6.8

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,12 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteForm />
+      <AnecdoteList />
+    </div>
+  )
+
+export default App
